ef084557d1f5063da0db7b06b1d98ef9265ceb7d
Abdul Samad <asbh.samad@gmail.com>
2024-07-02 10:15:59 +0200

    First Commit

--- lib/flutter_3d_objects.dart
+++ lib/flutter_3d_objects.dart
@@ -1,7 +1,14 @@
 library flutter_3d_objects;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/widget.dart';
+export 'src/scene.dart';
+export 'src/object.dart';
+export 'src/camera.dart';
+export 'src/mesh.dart';
+export 'src/material.dart';
+export 'package:vector_math/vector_math_64.dart';
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
